458b9aa10f9197e3538870ffa737ca443ef8fc36
Alberto Soriano <albertosoriano.e@gmail.com>
2024-05-21 23:10:51 -0500

    BREAKING CHANGE feat(): Nueva documentación

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
 from src.core.config.database import Base, engine
-from src.api.v1.routers.main_router import api_v1_router
+from src.api.v1.app import api_v1
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "v1",
+        "description": "API version 1, check link on the right",
+        "externalDocs": {
+            "description": "sub-docs",
+            "url": "http://127.0.0.1/api/v1/docs",
+        },
+    },
+    {
+        "name": "v2",
+        "description": "API version 2, check link on the right",
+        "externalDocs": {
+            "description": "sub-docs",
+            "url": "http://127.0.0.1/api/v2/docs",
+        },
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
 app.title = "Roulette UETS Service"
 app.version = "0.0.1"
 
@@ -17,12 +38,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_v1_router)
+app.mount("/api/v1", api_v1)
 
 Base.metadata.create_all(bind=engine)
 
 
-@app.get("/", tags=["Index"])
+@app.get("/", tags=["Index"], include_in_schema=False)
 async def index():
     return JSONResponse(
         content={"message": "Welcome to Roulette UETS Service"}, status_code=200
